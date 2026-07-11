4629fce497e42f6af11f27307abbba695536303a
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-11 18:01:35 +0300

    Better structure finished

---
+++ util/db.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize");
+const { DATABASE_URL } = require("./config");
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+});
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("connected to the database");
+  } catch (error) {
+    console.log("failed to connect to the database");
+    console.log(error);
+    return process.exit(1);
+  }
+
+  return null;
+};
+
+module.exports = { connectToDatabase, sequelize };
