e6db13369396a6780be1ba405bafd38f3419d314
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-11 21:01:56 +0300

    Let there be users finished

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const { PORT } = require("./util/config");
 const { connectToDatabase } = require("./util/db");
 const { errorHandler } = require("./util/middleware");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(errorHandler);
 
 const start = async () => {
