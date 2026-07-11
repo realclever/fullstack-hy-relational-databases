6ff8e57dec36e2d82d13b13060f9aa21f18ac64d
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-12 01:03:32 +0300

    Blog ownership finished

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,39 @@
+const jwt = require("jsonwebtoken");
 const router = require("express").Router();
-const { Blog } = require("../models");
+
+const { SECRET } = require("../util/config");
+const { Blog, User } = require("../models");
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch (error) {
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+
+  next();
+};
 
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", tokenExtractor, async (req, res, next) => {
   try {
-    const blog = await Blog.create(req.body);
+    const user = await User.findByPk(req.decodedToken.id);
+
+    const blog = await Blog.create({
+      ...req.body,
+      userId: user.id,
+    });
+
     res.json(blog);
   } catch (error) {
     next(error);
--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const { connectToDatabase } = require("./util/db");
 const { errorHandler } = require("./util/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(errorHandler);
 
 const start = async () => {
