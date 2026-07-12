8b5653fb0388a605365a3520c0d2e63e570325b3
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-12 18:32:37 +0300

    Remember the year finished

--- models/blog.js
+++ models/blog.js
@@ -25,6 +25,19 @@ Blog.init(
       type: DataTypes.INTEGER,
       defaultValue: 0,
     },
+    year: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: {
+          args: 1991,
+          msg: "year must be at least 1991",
+        },
+        max: {
+          args: new Date().getFullYear(),
+          msg: "year cannot be greater than the current year",
+        },
+      },
+    },
   },
   {
     sequelize,
