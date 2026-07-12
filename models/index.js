9686f575948d47a4ff188c6adab8f5f7367c94b0
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-13 00:35:08 +0300

    Reading list finished

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./reading_list");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+User.belongsToMany(Blog, { through: ReadingList, as: "readings" });
+Blog.belongsToMany(User, { through: ReadingList, as: "usersReading" });
+
 module.exports = {
   Blog,
   User,
+  ReadingList,
 };
