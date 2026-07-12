f400a99c96c3bfa88e3ed4c8ac8ede037c1a7d46
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-12 03:04:09 +0300

    Authors and bonus finished

---
+++ controllers/authors.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+const { sequelize } = require("../util/db");
+const { Blog } = require("../models");
+
+router.get("/", async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      "author",
+      [sequelize.fn("COUNT", sequelize.col("id")), "blogs"],
+      [sequelize.fn("SUM", sequelize.col("likes")), "likes"],
+    ],
+    group: ["author"],
+    order: [[sequelize.fn("SUM", sequelize.col("likes")), "DESC"]],
+    raw: true,
+  });
+
+  res.json(authors);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { errorHandler } = require("./util/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const authorsRouter = require("./controllers/authors");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/authors", authorsRouter);
 app.use(errorHandler);
 
 const start = async () => {
