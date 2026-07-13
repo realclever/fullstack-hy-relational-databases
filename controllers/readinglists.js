b9b9e0845be2c1ee7b9735981ea70060dcb8bf06
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-13 19:28:36 +0300

    Better bookkeeping finished

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const { ReadingList, User, Blog } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 router.post("/", async (req, res, next) => {
   try {
@@ -25,6 +26,37 @@ router.post("/", async (req, res, next) => {
       userId,
       blogId,
     });
+
+    res.json(readingList);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", tokenExtractor, async (req, res, next) => {
+  try {
+    const readingList = await ReadingList.findByPk(req.params.id);
+
+    if (!readingList) {
+      return res.status(404).json({
+        error: "reading list not found",
+      });
+    }
+
+    if (readingList.userId !== req.decodedToken.id) {
+      return res.status(403).json({
+        error: "forbidden",
+      });
+    }
+
+    if (typeof req.body.read !== "boolean") {
+      return res.status(400).json({
+        error: "read must be a boolean",
+      });
+    }
+
+    readingList.read = req.body.read;
+    await readingList.save();
 
     res.json(readingList);
   } catch (error) {
--- util/middleware.js
+++ util/middleware.js
@@ -1,3 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("./config");
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({
+        error: "token invalid",
+      });
+    }
+  } else {
+    return res.status(401).json({
+      error: "token missing",
+    });
+  }
+
+  next();
+};
+
 const errorHandler = (error, req, res, next) => {
   console.error(error.message);
 
@@ -14,5 +37,6 @@ const errorHandler = (error, req, res, next) => {
 };
 
 module.exports = {
+  tokenExtractor,
   errorHandler,
 };
